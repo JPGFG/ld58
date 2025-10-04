e4b615a6dd04b8555bcda0d7d0b90049e1c74773
STR <str5evera@gmail.com>
2025-10-04 15:32:34 -0400

    trying to figure out worm collisions

--- scripts/worm_movement.gd
+++ scripts/worm_movement.gd
@@ -5,9 +5,12 @@ extends CharacterBody2D
 
 var is_caught: bool = false
 var start_pos : Vector2
+var worm_shape: Shape2D
 
 func _ready() -> void:
 	start_pos = position
+	var collision_shape = $CollisionShape2D
+	worm_shape = collision_shape.shape
 	
 func _physics_process(delta: float) -> void:
 	if is_caught:
@@ -23,10 +26,7 @@ func move_horizontal(delta: float):
 	position = Vector2(new_x, 0)
 
 func move_vertical(delta: float):
-	# move top→bottom, sine wave in X
 	var new_y = position.y + -speed * delta
-	#velocity = Vector2(0, 5)
-	#position = Vector2(position.x, new_y)
 	position.y = new_y
 	
 func caught_in_web():
@@ -40,5 +40,37 @@ func wiggle(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("anchor"):
+	# Get the worm's shape
+	var worm_shape = get_node("CollisionShape2D").shape as RectangleShape2D
+	var worm_half_width = worm_shape.size.x / 2
+	var worm_half_height = worm_shape.size.y / 2
+	var worm_center = global_position
+
+	# Worm corners
+	var worm_top_left = Vector2(worm_center.x - worm_half_width, worm_center.y - worm_half_height)
+	var worm_top_right = Vector2(worm_center.x + worm_half_width, worm_center.y - worm_half_height)
+	var worm_bottom_left = Vector2(worm_center.x - worm_half_width, worm_center.y + worm_half_height)
+	var worm_bottom_right = Vector2(worm_center.x + worm_half_width, worm_center.y + worm_half_height)
+
+	# anchor shape
+	var anchor_shape = body.get_node("CollisionShape2D").shape as RectangleShape2D
+	var anchor_half_width = anchor_shape.size.x / 2
+	var anchor_half_height = anchor_shape.size.y / 2
+	var anchor_center = body.global_position
+	# anchor edges
+	var anchor_left = anchor_center.x - anchor_half_width
+	var anchor_right = anchor_center.x + anchor_half_width
+	var anchor_top = anchor_center.y - anchor_half_height
+	var anchor_bottom = anchor_center.y + anchor_half_height
+
+	# if the worm's top is above the anchor's top start horizontal movement
+	if worm_top_left.y <= anchor_top and worm_top_right.y <= anchor_top:
+		movement_axis = "Horizontal"
+
+	# if the worm is within horizontal bounds of the anchor, and hits vertical bounds go vertical
+	elif (worm_bottom_left.x <= anchor_right and worm_bottom_right.x >= anchor_left) and worm_bottom_left.y >= anchor_top:
 		movement_axis = "Vertical"
+		
+	#if body.is_in_group("anchor"):
+		#movement_axis = "Vertical"
+	
